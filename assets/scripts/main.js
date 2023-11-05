72db0bb35b35d334ca7380177a3e6012ba013150
Vladimir <daintymonk@gmail.com>
2023-11-05 13:44:18 +0300

    fix: anchor scrolling

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,26 @@
-const hamb = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".header__nav-menu");
+const navMenu = document.querySelector('.header__nav-menu');
+const links = document.querySelectorAll('.header__nav-link');
+const hamb = document.querySelector('.hamburger');
 
-hamb.addEventListener("click", () => {
-  hamb.classList.toggle("active");
-  navMenu.classList.toggle("active");
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const href = link.getAttribute('href').substring(1);
+    const scrollTarget = document.getElementById(href);
+    const topOffset = document.querySelector('.header').offsetHeight;
+
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth',
+    })
+  })
+})
+
+hamb.addEventListener('click', () => {
+  hamb.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
