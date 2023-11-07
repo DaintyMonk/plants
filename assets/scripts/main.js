c454d7f44cec9600a9090caa32d7a2571697d83d
Vladimir <daintymonk@gmail.com>
2023-11-08 00:25:31 +0300

    feat: add select

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,8 @@ const hamb = document.querySelector('.hamburger');
 const cards = document.querySelectorAll('.service__card');
 const accordions = document.querySelectorAll('.prices__accordion');
 const pricesAccordionBlock = document.querySelector('.prices__accordion-block');
+const select = document.querySelector('.contacts__select');
+const addressCards = document.querySelectorAll('.contacts__address-card');
 
 // Smooth scrolling
 links.forEach(link => {
@@ -32,35 +34,68 @@ hamb.addEventListener('click', () => {
 
 // Card blur effect
 const focusChange = (btn) => {
-  const unblurTarget = btn.innerHTML.slice(0,4);
   const activeButton = document.querySelector('.btn_active');
   if (activeButton != null) {
     activeButton.classList.toggle('btn_active');
   }
   btn.classList.toggle('btn_active');
+
   const activeCards = document.querySelectorAll('.card_active');
   activeCards.forEach(card => {
     card.classList.toggle('card_active');
   });
+
+  const unblurTarget = btn.innerHTML.slice(0,4);
   cards.forEach(card => {
     const cardType = card.querySelector('.card__img-text').innerHTML; 
     if (cardType.includes(unblurTarget)){
       card.classList.toggle('card_active');
     }
-  });
+  })
 }
 
+// Accordion
 accordions.forEach(accordion => {
   accordion.querySelector('.accordion__items').addEventListener('click', () => {
     accordion.classList.toggle('accordion_active');
+
     const activeAccordions = document.querySelectorAll('.accordion_active');
-    console.log(activeAccordions);
     if (!pricesAccordionBlock.classList.contains('accordion-block_active') && activeAccordions != 0) {
-      pricesAccordionBlock.classList.toggle('accordion-block_active');
+      pricesAccordionBlock.classList.add('accordion-block_active');
     }
     else if (activeAccordions.length === 0) {
-      pricesAccordionBlock.classList.toggle('accordion-block_active');
+      pricesAccordionBlock.classList.remove('accordion-block_active');
     }
+  })
+})
+
+// Custom select
+select.querySelector('.select__input').addEventListener('click', () => {
+  const addressActive = document.querySelector('.address-card_active');
+
+  if (addressActive != null) {
+    addressActive.classList.remove('address-card_active');
+  } 
+
+  select.classList.toggle('select_active');
+
+  if (!select.classList.contains('select_active')) {
+    const selectText = select.querySelector('.select__text').innerText;
+    if (selectText != 'City') {
+      document.getElementById(selectText).classList.add('address-card_active');
+    }
+  }
+})
+
+// Address selection
+select.querySelectorAll('.select__dropdown-item').forEach(selectItem => {
+  selectItem.addEventListener('click', () => {
+    select.querySelector('.select__text').innerText = selectItem.innerText;
+
+    select.classList.remove('select_active');
+  
+    const selectCity = selectItem.getAttribute('data-value');
+    document.getElementById(selectCity).classList.add('address-card_active');
   })
 })
 
